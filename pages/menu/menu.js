cb7f0c432f81f8e4e5bd286dff23d351b4c7d6fc
Wycer <734849165@qq.com>
2019-06-23 18:23:04 +0800

    fix gpa calculation problem & format

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -3,19 +3,19 @@ const app = getApp();
 let that = this;
 Page({
   data: {
-    tabs: ["GPA","设置"],
+    tabs: ["GPA", "设置"],
     activeIndex: 0,
     sliderOffset: 0,
-    sliderLeft: 0, 
-    gpa:0,
+    sliderLeft: 0,
+    gpa: 0,
     periods: ['全部学期', '2018-2019-2', '2018-2019-1', '2017-2018-3', '2017-2018-2', '2017-2018-1', '2016-2017-3', '2016-2017-2', '2016-2017-1', '2015-2016-3', '2015-2016-2', '2015-2016-1', '2014-2015-3', '2014-2015-2', '2014-2015-1', '2013-2014-2', '2013-2014-1', '2012-2013-2', '2012-2013-1', '2011-2012-2', '2011-2012-1', '2010-2011-2', '2010-2011-1'],
-    periodIndex:0,
-    checkboxItems: getApp().globalData.defaultItems 
+    periodIndex: 0,
+    checkboxItems: getApp().globalData.defaultItems
   },
-  onReady:function(){
+  onReady: function () {
     let that = this;
     that.setData({ checkboxItems: getApp().globalData.defaultItems });
-    that.setData({gpa:that.calculateGPA().toFixed(2)});
+    that.setData({ gpa: that.calculateGPA().toFixed(2) });
   },
   onLoad: function () {
     var that = this;
@@ -27,15 +27,7 @@ Page({
           sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
         });
       }
-
-
     });
-  
-  
-    
-   
-
-   
   },
   tabClick: function (e) {
     console.log(this.checkboxItems);
@@ -45,49 +37,47 @@ Page({
     });
 
   },
-  bindPeriodChange: function(e){
-  //  let that = this;
+  bindPeriodChange: function (e) {
+    //  let that = this;
     this.setData({
-          periodIndex:e.detail.value
+      periodIndex: e.detail.value
     })
     var items = [];
     var size = 0;
-      if(this.data.periods[this.data.periodIndex] != "全部学期"){
-          for (var key in app.globalData.grade[this.data.periods[this.data.periodIndex]]) {
-            if (typeof (key) != undefined) {
-              var temp_str = app.globalData.grade[this.data.periods[this.data.periodIndex]][key][0] + " " + app.globalData.grade[this.data.periods[this.data.periodIndex]][key][1];
-              console.log(temp_str);
-              items.push({ name: key, num: app.globalData.grade[this.data.periods[this.data.periodIndex]][key][1], grade: temp_str, credit: app.globalData.grade[this.data.periods[this.data.periodIndex]][key][2], value: String(size), checked: true });
-              size = size + 1;
-            }
-
-          }
-    }
-    else{
-        for (var i = 0; i < app.globalData.periods.length; i++) {
-          //     console.log(app.globalData.periods[i]);
-
-          for (var key in app.globalData.grade[app.globalData.periods[i]]) {
-            if (typeof (key) != undefined) {
-              //    console.log(key);
-              // console.log(app.globalData.grade[app.globalData.periods[i]][key][1]);
-              items.push({ name: key, num: app.globalData.grade[app.globalData.periods[i]][key][1], grade: app.globalData.grade[app.globalData.periods[i]][key][0] + " " + app.globalData.grade[app.globalData.periods[i]][key][1], credit: app.globalData.grade[app.globalData.periods[i]][key][2], value: String(size), checked: true });
-              size = size + 1;
-            }
-
-          }
+    if (this.data.periods[this.data.periodIndex] != "全部学期") {
+      for (var key in app.globalData.grade[this.data.periods[this.data.periodIndex]]) {
+        if (typeof (key) != undefined) {
+          var temp_str = app.globalData.grade[this.data.periods[this.data.periodIndex]][key][0] + " " + app.globalData.grade[this.data.periods[this.data.periodIndex]][key][1];
+          console.log(temp_str);
+          items.push({ name: key, num: app.globalData.grade[this.data.periods[this.data.periodIndex]][key][1], grade: temp_str, credit: app.globalData.grade[this.data.periods[this.data.periodIndex]][key][2], value: String(size), checked: true });
+          size = size + 1;
         }
+
+      }
+    }
+    else {
+      for (var i = 0; i < app.globalData.periods.length; i++) {
+        //     console.log(app.globalData.periods[i]);
+
+        for (var key in app.globalData.grade[app.globalData.periods[i]]) {
+          if (typeof (key) != undefined) {
+            //    console.log(key);
+            // console.log(app.globalData.grade[app.globalData.periods[i]][key][1]);
+            items.push({ name: key, num: app.globalData.grade[app.globalData.periods[i]][key][1], grade: app.globalData.grade[app.globalData.periods[i]][key][0] + " " + app.globalData.grade[app.globalData.periods[i]][key][1], credit: app.globalData.grade[app.globalData.periods[i]][key][2], value: String(size), checked: true });
+            size = size + 1;
+          }
+
+        }
+      }
     }
     console.log(items);
     console.log(this.data.periodIndex);
-    this.setData({checkboxItems:items});
+    this.setData({ checkboxItems: items });
     console.log(this.data.checkboxItems);
-    this.setData({ gpa: this.calculateGPA().toFixed(2)});
+    this.setData({ gpa: this.calculateGPA().toFixed(2) });
     this.onLoad();
   },
   checkboxChange: function (e) {
- //   console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
     var checkboxItems = this.data.checkboxItems, values = e.detail.value;
     for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
       checkboxItems[i].checked = false;
@@ -104,68 +94,38 @@ Page({
       checkboxItems: checkboxItems
     });
     this.setData(
-      {gpa: this.calculateGPA().toFixed(2)}
+      { gpa: this.calculateGPA().toFixed(2) }
     );
   },
-  calculateGPA:function(){
-      var res = 0.0;
-      var count = 0;
-      var totalCredit = 0;
-      for(var i = 0 ; i < this.data.checkboxItems.length; i ++){
-          var temp = parseFloat(this.data.checkboxItems[i]['num']);
-          console.log(temp);
-          if(!isNaN(temp) &&  this.data.checkboxItems[i].checked == true){
-            if(temp > 60){
-              count++;
-              if (temp >= 97) {
-                res += 4.00 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 93) {
-                res += 3.94 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 90) {
-                res += 3.85 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 87) {
-                res += 3.73 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 83) {
-                res += 3.55 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 80) {
-                res += 3.32 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 77) {
-                res += 3.09 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 73) {
-                res += 2.78 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 70){
-                res += 2.42 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 67) {
-                res += 2.42 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else if (temp >= 63) {
-                res += 1.63 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              else {
-                res += 1.15 * parseFloat(this.data.checkboxItems[i].credit);
-              }
-              
-              totalCredit += parseFloat(this.data.checkboxItems[i].credit);
-            }
-          
-          }
-      }
-      if(count == 0){
-          return 0.00;
-      }
-      else{
-          console.log(res / totalCredit);
-      return res / totalCredit;
-      }
-  }
+  calculateGPA: function () {
+    const tmp = this.data.checkboxItems.filter(
+      course =>
+        course.checked &&
+        /^[0-9]+$/.test(course.num) &&
+        parseInt(course.num) >= 60
+    )
 
+    let res = 0.0
+    let totalCredit = 0
+    if (!tmp.length) return res
+
+    tmp.forEach(course => {
+      const score = parseFloat(course.num)
+      const credit = parseFloat(course.credit)
+      if (score >= 97) res += 4.0 * credit
+      else if (score >= 93) res += 3.94 * credit
+      else if (score >= 90) res += 3.85 * credit
+      else if (score >= 87) res += 3.73 * credit
+      else if (score >= 83) res += 3.55 * credit
+      else if (score >= 80) res += 3.32 * credit
+      else if (score >= 77) res += 3.09 * credit
+      else if (score >= 73) res += 2.78 * credit
+      else if (score >= 70) res += 2.42 * credit
+      else if (score >= 67) res += 2.08 * credit
+      else if (score >= 63) res += 1.63 * credit
+      else res += 1.15 * credit
+      totalCredit += credit
+    })
+    return res / totalCredit
+  }
 });
